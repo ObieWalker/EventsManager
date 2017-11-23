8428d70514fca58fe652d347c2d5b49d539ebb96
ObieWalker <obinnawalker@gmail.com>
2017-11-23 19:43:46 +0100

    added API feature

---
+++ server/controllers/controllerCE.js
@@ -0,0 +1,93 @@
+import {eventData} from '../models/dummyEvents';
+import {centerData} from '../models/dummyCenters';
+
+
+export default class Events {
+
+  constructor(req, res) {
+    this.req = req;
+    this.res = res;
+  }
+
+  createEvent(info) {
+    const id = (dummyEvents[dummyEvents.length - 1].eventId) + 1;
+    const newEvent = {
+      eventId: id,
+      eventType: info.type
+      eventName: info.name,
+      Location: info.location,
+      guestNo: info.guestNo,
+      eventDate: info.eventDate,
+      facility: info.facility
+    };
+    console.log(newEvent);
+    dummyEvents.push(newEvent);
+    this.res.status(201).json({val: newEvent});
+  }
+
+  //this will return all the events in dummy data
+  getAllEvents() {
+    this.res.status(200).json({val: dummyEvents});
+  }
+  deleteEvent(id) {
+    const matchedIndex = dummyEvents.findIndex(e => e.eventId === parseInt(id, 10));
+    dummyEvents.splice(matchedIndex, 1);
+    this.res.status(200).json({message: 'Your event has been deleted'});
+  }
+
+  //modify an event based on 
+  modifyEvent(id, data) {
+    //matches an index of the input id
+    const matchedIndex = dummyEvents.findIndex(e => e.eventId === parseInt(id, 10));
+    dummyEvents[matchedIndex].eventType = data.type;
+    dummyEvents[matchedIndex].eventName = data.name;
+    dummyEvents[matchedIndex].Location = data.location;
+    dummyEvents[matchedIndex].guestNo = data.guestNo;
+    dummyEvents[matchedIndex].eventDate = data.date;
+    dummyEvents[matchedIndex].facility = data.facility;
+    this.res.status(200).json(dummyEvents[dataIndex]);
+  }  
+
+  //get a centers details
+  getDetails(id) {
+    const matched = dummyEvents.filter(e => e.eventId === parseInt(id, 10));
+    console.log(matched);
+    this.res.status(200).json({val: matched});
+  }
+  addCenter(info) {
+    const id = (dummyCenters[dummyCenters.length - 1].eventId) + 1;
+    const newCenter = {
+      centerId: id,
+      centerName : info.name,
+      Location: info.location,
+      capacity: info.capacity,
+      facility: info.facility
+    };
+    console.log(newCenter);
+    dummyCenters.push(newCenter);
+    this.res.status(201).json({val: newEvent});    
+  }
+  getAllCenters(){
+    this.res.status(200).json({val: dummyCenters});
+  }
+  modifyCenter(id, info){
+    const matchedIndex = dummyCenters.findIndex(c => c.centerId === parseInt(id,10));
+    dummyCenters[matchedIndex].centerName = info.name;
+    dummyCenters[matchedIndex].Location = info.location;
+    dummyCenters[matchedIndex].capacity = info.capacity;
+    dummyCenters[matchedIndex].facility = info.facility;
+    this.res.status(200).json(dummyCenters[matchedIndex]);
+  }
+  deleteCenter(id){
+    const matchedIndex = dummyCenters.findIndex(c => c.centerId === parseInt(id, 10));
+    dummyCenters[matchedIndex].centerName = data.name;
+    dummyCenters[matchedIndex].Location = data.location;
+    dummyCenters[matchedIndex].capacity = data.capacity;
+    dummyCenters[matchedIndex].facility = data.facility;
+
+  }
+
+
+}
+
+ 
---
+++ server/models/dummyCenters.js
@@ -0,0 +1,29 @@
+export const dummyCenters = [
+{
+  centerId: 1001,
+  centerName: 'sunset Villa',
+  Location: 'Victoria Island, Lagos',
+  capacity: 500,
+  facility: ['Projector', 'Stage']
+},
+{
+  centerId: 1002,
+  centerName: 'Civic Hills',
+  Location: 'Lekki, Lagos',
+  capacity: 250
+  facility: ['Projector', 'Stage', 'Swimming Pool']
+},
+{
+  centerId: 1003,
+  centerName: 'Havana Havilla',
+  Location: 'VGC, Lagos',
+  capacity: 150,
+  facility: ['Projector', 'Stage', 'Swimming Pool', 'Quickie Room']
+},
+{
+  centerId: 1004,
+  centerName: 'The Plug Spot',
+  Location: 'Marwa, Lagos',
+  capacity: 100,
+  facility: ['Ball Room', 'Stage', 'Backstage', 'Mezzanine']
+}];
