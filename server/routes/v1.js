0aff2580e5d5c979d14120399fd325aa4345f327
ObieWalker <obinnawalker@gmail.com>
2017-12-01 08:32:17 +0100

    add dotenv to app

--- server/routes/v1.js
+++ server/routes/v1.js
@@ -1,23 +1,25 @@
-import express from 'express';
-import UsersController from '../controllers/usersController';
-import EventsController from '../controllers/eventsController';
-import CentersController from '../controllers/centersController';
+import express from 'express'
+import dotenv from 'dotenv'
+import UsersController from '../controllers/usersController'
+import EventsController from '../controllers/eventsController'
+import CentersController from '../controllers/centersController'
 
 // versioning our api
-const apiv1 = express.Router();
+const apiv1 = express.Router()
 
 // account creation and login
 apiv1.post('/users/', UsersController.signup)
 apiv1.post('/users/login', UsersController.signin)
 
+dotenv.config()
 // create a new event
-apiv1.post('/events', EventsController.createEvent);
+apiv1.post('/events', EventsController.createEvent)
 // return a users events
 //apiv1.get('/users/:userId/events', EventsController.allUsersEvents);
 // modify an event
-apiv1.put('/events/:id', EventsController.editEvent);
+apiv1.put('/events/:id', EventsController.editEvent)
 // delete an event
-apiv1.delete('/events/:id', EventsController.deleteEvent);
+apiv1.delete('/events/:id', EventsController.deleteEvent)
 
 // add new center
 apiv1.post('/centers', CentersController.createCenter)
