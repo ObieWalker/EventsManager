fdf6af802c10703b4add1de43237a578294b4d8b
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2018-05-13 22:29:14 +0100

    chore(feedback implementation): implement feedback from defence - add check to ensure user exists in token authentication - add catch all for invalid URL requests - update readme with more information - add license - add contributing.md file [finishes #157542235]

--- server/middlewares/authenticator.js
+++ server/middlewares/authenticator.js
@@ -1,8 +1,24 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import { User } from '../models';
 
 dotenv.config();
+/**
+ * @description authenticator
+ *
+ * @export
+ * @class auth
+ */
 export default class auth {
+  /**
+   * @returns {object} message
+   *
+   * @static
+   * @param {any} req
+   * @param {any} res
+   * @param {any} next
+   * @memberof auth
+   */
   static authenticate(req, res, next) {
     const token = req.body.token || req.query.token || req.headers.token;
     if (token) {
@@ -10,9 +26,18 @@ export default class auth {
         if (err) {
           return res.status(401).json({ message: 'Failed to authenticate user' });
         }
-        // the user data is stored
-        req.decoded = decoded;
-        next();
+        User.findOne({
+          where: {
+            id: decoded.id
+          }
+        }).then((user) => {
+          if (!user) {
+            return res.status(401).json({ message: 'User account does not exist' });
+          }
+          // the user data is stored
+          req.decoded = decoded;
+          next();
+        });
       });
     } else {
       res.status(403).json({ message: 'No token, please sign up or sign in' });
