b889fc97dd5bb9ba4f806aad30bc5fad42f71061
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-5.local>
2018-05-25 17:01:55 +0100

    .

--- server/seeders/20180404155309-demo-user.js
+++ server/seeders/20180404155309-demo-user.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 const bcrypt = require('bcrypt');
 
 module.exports = {
--- server/tests/index.js
+++ server/tests/index.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 import users from './users';
 import centers from './centers';
 import events from './events';
